179df9b4ebb9ace7a5f2feb7eec6bd9e86c2eacf
Gustavo Henrique da Silva <sykogus@gmail.com>
2024-02-10 00:53:35 -0300

    Criado

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Horários</h1>
+
+    <b-table 
+      :items="tableData" 
+      :fields="tableFields"
+      >
+
+    </b-table>
+
+
+  </div>
+</template>
+
+<script>
+
+import { BTable } from 'bootstrap-vue'
+
+// verificar outro componente bootstrap para fazer as linhas ( group item talvez algo assims)
+
+
+export default {
+  name: 'HelloWorld',
+
+  data() {
+    return {
+      tableData: [
+        { id: 1, name: 'Alice', age: 25 },
+        { id: 2, name: 'Bob', age: 30 },
+        // Mais dados aqui...
+      ],
+      tableFields: [
+        { key: 'name', label: 'Nome' },
+        { key: 'age', label: 'Idade' },
+        // Mais campos aqui...
+      ],
+    };
+  },
+
+  components: {
+    BTable,
+
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
